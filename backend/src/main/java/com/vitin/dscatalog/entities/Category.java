fd07ab7e43e98211122dfd2486afd73871a8c5da
Victor <victorgaspalmeida@hotmail.com>
2023-03-11 11:08:32 -0300

    first commit

---
+++ backend/src/main/java/com/vitin/dscatalog/entities/Category.java
@@ -0,0 +1,54 @@
+package com.vitin.dscatalog.entities;
+
+import java.util.Objects;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Category() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
