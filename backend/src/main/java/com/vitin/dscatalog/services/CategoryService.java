806fad795e734ababa0be2bd5d433a28bcb0045f
Victor <victorgaspalmeida@hotmail.com>
2023-03-12 19:04:29 -0300

    dto

--- backend/src/main/java/com/vitin/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vitin/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.vitin.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vitin.dscatalog.dto.CategoryDTO;
 import com.vitin.dscatalog.entities.Category;
 import com.vitin.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(item -> new CategoryDTO(item)).collect(Collectors.toList());
 	}
 }
