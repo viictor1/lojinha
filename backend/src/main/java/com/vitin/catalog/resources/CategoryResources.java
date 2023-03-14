a3e2ba3f205ddca1829b8b65f1aa95af7bfc424c
Victor <victorgaspalmeida@hotmail.com>
2023-03-14 11:42:53 -0300

    find by id e tratamento de execoes

---
+++ backend/src/main/java/com/vitin/catalog/resources/CategoryResources.java
@@ -0,0 +1,34 @@
+package com.vitin.catalog.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vitin.catalog.dto.CategoryDTO;
+import com.vitin.catalog.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();	
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+}
